e1e12c7d506569ec12884ff2892ea3fff1962227
kurth4cker <kurth4cker@gmail.com>
2024-02-21 14:04:46 +0300

    dynarr.h: Rename vector to dynarr and use pragma once

--- dynarr.h
+++ dynarr.h
@@ -1,14 +1,10 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#pragma once
+
 #include <stddef.h>
 
-struct vector {
+struct dynarr {
 	void *data;
 	size_t size; /* sizeof each element */
 	size_t len; /* number of elements */
 	size_t capacity; /* element capacity */
 };
-
-int vector_init(struct vector *, size_t);
-
-#endif /* VECTOR_H */
