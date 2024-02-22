22ebef96472228993aa4789c7134cda5be052a4f
kurth4cker <kurth4cker@gmail.com>
2024-02-22 14:47:42 +0300

    Update tests

---
+++ dynarr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+#include "dynarr.h"
+
+void *
+dynarr_get(const struct dynarr *vec, size_t idx)
+{
+	if (idx >= vec->len)
+		return NULL;
+
+	return (char *)vec->data + idx * vec->size;
+}
+
+int
+dynarr_init(struct dynarr *vec, size_t size)
+{
+	static const size_t capacity = 64;
+
+	vec->data = calloc(64, size);
+	if (!vec->data)
+		return -1;
+
+	vec->size = size;
+	vec->len = 0;
+	vec->capacity = capacity;
+
+	return 0;
+}
+
+int
+dynarr_push(struct dynarr *vec, const void *obj)
+{
+	(void)vec;
+	(void)obj;
+	return 0;
+}
+
+void
+dynarr_term(struct dynarr *vec)
+{
+	free(vec->data);
+	vec->data = NULL;
+	vec->capacity = 0;
+	vec->size = 0;
+	vec->len = 0;
+}
