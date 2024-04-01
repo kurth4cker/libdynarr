58997764a8d9bc897b9e63e71bf200e9feea501f
kurth4cker <kurth4cker@gmail.com>
2024-04-02 00:41:58 +0300

    test: Add push tests

--- test/main.c
+++ test/main.c
@@ -38,8 +38,42 @@ test_new(const MunitParameter *params, void *fixture)
 	return MUNIT_OK;
 }
 
+static MunitResult
+test_push(const MunitParameter *params, void *fixture)
+{
+	(void)params;
+	(void)fixture;
+	struct dynarr *arr = dynarr_new(sizeof(int));
+	if (!arr)
+		return MUNIT_ERROR;
+
+	int i = 42;
+	dynarr_push(arr, &i);
+	assert_size(arr->len, ==, 1);
+	assert_int(*(int *)arr->data, ==, 42);
+
+	i = 43;
+	dynarr_push(arr, &i);
+	assert_size(arr->len, ==, 2);
+	assert_int(*(int *)arr->data, ==, 42);
+	assert_int(*(int *)(arr->data + arr->size), ==, 43);
+
+	i = 44;
+	dynarr_push(arr, &i);
+	assert_size(arr->len, ==, 3);
+	assert_int(*(int *)arr->data, ==, 42);
+	assert_int(*(int *)(arr->data + arr->size), ==, 43);
+	assert_int(*(int *)(arr->data + 2 * arr->size), ==, 44);
+
+	free(arr->data);
+	free(arr);
+
+	return MUNIT_OK;
+}
+
 static MunitTest tests[] = {
 	{ "/new", test_new, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/push", test_push, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 	{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
 
