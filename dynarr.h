a1fad77923cf1b5b9faea7cf29b53a5447b03444
kurth4cker <kurth4cker@gmail.com>
2025-04-25 11:04:39 +0300

    dynarr: move data to end of struct

--- dynarr.h
+++ dynarr.h
@@ -3,10 +3,10 @@
 #include <stddef.h>
 
 struct dynarr {
-	void *data;
 	size_t size; /* sizeof each element */
 	size_t len; /* number of elements */
 	size_t capacity; /* element capacity */
+	void *data;
 };
 
 struct dynarr *dynarr_new(size_t);
