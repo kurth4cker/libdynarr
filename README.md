405035611fdc47ed6f795aa1e9640ae3d9c13e66
kurth4cker <kurth4cker@gmail.com>
2025-04-30 22:39:08 +0300

    build: migrate to nob.h
    Fixes: #4

--- README.md
+++ README.md
@@ -4,25 +4,20 @@ Simple dynamic array implementation in C. [WIP]
 ## Building
 Standart Makefile. Here are building and testing samples:
 
-    # build library
-    make
+    # bootstrap build system
+    cc -o nob nob.c
 
-    # run test suite
-    make check
+    # build the library
+    ./nob
 
-    # install library
-    make install
 
 ## Using Library
 Just simple library. Use it as system library or copy both of dynarr.c and
 dynarr.h into your project tree.
 
-    # as system library
-    # requires libdynarr to be installed on your system at building
-    cc -o sample main.c -ldynarr
-
     # copy it to your source tree
     cc -o sample main.c dynarr.c
+
 
 ## Copying
 Licensed under the terms of GPL-3.0-or-later. See file COPYING for details.
---
+++ nob.c
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+#include <stdlib.h>
+
+#define NOB_EXPERIMENTAL_DELETE_OLD
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+static void
+cc_and_cflags(Nob_Cmd *cmd)
+{
+	nob_cmd_append(cmd, "cc", "-std=c17");
+	nob_cmd_append(cmd, "-pedantic");
+	nob_cmd_append(cmd, "-Wall", "-Wextra", "-Werror");
+	nob_cmd_append(cmd, "-g");
+}
+
+// TODO: add install/uninstall support
+int
+main(int argc, char **argv)
+{
+	NOB_GO_REBUILD_URSELF(argc, argv);
+
+	Nob_Cmd cmd = { 0 };
+
+	cc_and_cflags(&cmd);
+	nob_cc_inputs(&cmd, "-c", "dynarr.c");
+	nob_cc_output(&cmd, "dynarr.o");
+	if (!nob_cmd_run_sync_and_reset(&cmd)) {
+		exit(EXIT_FAILURE);
+	}
+
+	nob_cmd_append(&cmd, "ar", "-rc");
+	nob_cmd_append(&cmd, "libdynarr.a", "dynarr.o");
+	if (!nob_cmd_run_sync_and_reset(&cmd)) {
+		exit(EXIT_FAILURE);
+	}
+
+	nob_cmd_append(&cmd, "ranlib", "libdynarr.a");
+	nob_cmd_run_sync_and_reset(&cmd);
+
+	cc_and_cflags(&cmd);
+	nob_cc_inputs(&cmd, "example.c", "libdynarr.a");
+	nob_cc_output(&cmd, "example");
+	if (!nob_cmd_run_sync_and_reset(&cmd)) {
+		exit(EXIT_FAILURE);
+	}
+}
